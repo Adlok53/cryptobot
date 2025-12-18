dfcb3e86d089ccccd9d0d49d18448093be02d538
Adlok53 <01mikinini@gmail.com>
2025-12-18 03:20:14 +0300

    Изучил фильтры и работу с сообщениями

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv, dotenv_values
 
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 import logging
@@ -18,14 +18,28 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Hello world")
+    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
+
+@dp.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("Это команда /help")
+
+@dp.message(F.text == "Как дела?")
+async def how_are_you(message: Message):
+    await message.answer("OK!")
+
+@dp.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo="AgACAgIAAxkBAAMTaUNGaO90LvuopgcM75C6F2FLTswAAhYUaxuF6hlKmq6KHxgqs6QBAAMCAAN5AAM2BA",
+                               caption="Единорог")
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
 
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print('THE END')
+    asyncio.run(main())
