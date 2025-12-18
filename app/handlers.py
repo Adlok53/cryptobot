e72036c73f7f59a860b48c7d56091bf25e8b7eda
Adlok53 <01mikinini@gmail.com>
2025-12-18 06:57:49 +0300

    Added a router for the bot

---
+++ app/handlers.py
@@ -0,0 +1,26 @@
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
+
+@router.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("Это команда /help")
+
+@router.message(F.text == "Как дела?")
+async def how_are_you(message: Message):
+    await message.answer("OK!")
+
+@router.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo="AgACAgIAAxkBAAMTaUNGaO90LvuopgcM75C6F2FLTswAAhYUaxuF6hlKmq6KHxgqs6QBAAMCAAN5AAM2BA",
+                               caption="Единорог")
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
--- run.py
+++ run.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv, dotenv_values
 
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher
 
 import logging
+
+from app.handlers import router
 
 # Load variables from the .env file
 load_dotenv()
@@ -16,28 +16,8 @@ BOT_TOKEN = os.getenv("TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
-
-@dp.message(Command("help"))
-async def get_help(message: Message):
-    await message.answer("Это команда /help")
-
-@dp.message(F.text == "Как дела?")
-async def how_are_you(message: Message):
-    await message.answer("OK!")
-
-@dp.message(Command('get_photo'))
-async def get_photo(message: Message):
-    await message.answer_photo(photo="AgACAgIAAxkBAAMTaUNGaO90LvuopgcM75C6F2FLTswAAhYUaxuF6hlKmq6KHxgqs6QBAAMCAAN5AAM2BA",
-                               caption="Единорог")
-
-@dp.message(F.photo)
-async def get_photo(message: Message):
-    await message.answer(f'ID фото: {message.photo[-1].file_id}')
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
