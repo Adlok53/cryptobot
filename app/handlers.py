fce21ae415236342898e21c7dc961f830ba5fc08
Adlok53 <01mikinini@gmail.com>
2025-12-21 08:35:38 +0300

    Added a keyboard to the project

--- app/handlers.py
+++ app/handlers.py
@@ -2,11 +2,14 @@ from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
+import app.keyboards as kb
+
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
+    await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}",
+                        reply_markup=await kb.inline_cars())
 
 @router.message(Command("help"))
 async def get_help(message: Message):
---
+++ app/keyboards.py
@@ -0,0 +1,19 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton)
+
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Каталог')],
+    [KeyboardButton(text='Корзина'), KeyboardButton(text='Контакты')]
+],  resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню.')
+
+settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='YouTube', url='https://www.youtube.com')]])
+
+cars = ['Tesla', 'Mercedes', 'BMW', 'Porsche']
+
+async def inline_cars():
+    keyboard = InlineKeyboardBuilder()
+    for car in cars:
+        keyboard.add(InlineKeyboardButton(text=car, url='https://www.youtube.com'))
+    return keyboard.adjust(2).as_markup()
