2791a4f80ad05f8070327ffa31f3146d11a15030
Adlok53 <01mikinini@gmail.com>
2025-12-21 09:06:51 +0300

    CallbackQuery

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import app.keyboards as kb
 
@@ -9,7 +9,7 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply(f"Привет. \nТвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}",
-                        reply_markup=await kb.inline_cars())
+                        reply_markup=kb.main)
 
 @router.message(Command("help"))
 async def get_help(message: Message):
@@ -27,3 +27,8 @@ async def get_photo(message: Message):
 @router.message(F.photo)
 async def get_photo(message: Message):
     await message.answer(f'ID фото: {message.photo[-1].file_id}')
+
+@router.callback_query(F.data == 'catalog')
+async def catalog(callback: CallbackQuery):
+    await callback.answer('Вы выбрали каталог', show_alert=True)
+    await callback.message.edit_text('Привет!', reply_markup=await kb.inline_cars())
